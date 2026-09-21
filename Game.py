5f037edfbacb5d884ab2f49de7f9aaf7537398a8
yingxin <inez.yingxin@gmail.com>
2026-09-21 15:07:39 +0800

    added dice effect

--- Game.py
+++ Game.py
@@ -1,11 +1,28 @@
 from sense_hat import SenseHat
 import random
+import time
+
 sense = SenseHat()
 
 while True:
     for event in sense.stick.get_events():
         if event.action == "pressed" and event.direction == "middle":
+
+            # Dice rolling animation
+            for i in range(10):
+                dice = random.randint(1, 6)
+                sense.show_message(
+                    str(dice),
+                    text_colour=(255, 255, 255),
+                    scroll_speed=0.05
+                )
+                time.sleep(0.1)
+
+            # Final dice result
             dice = random.randint(1, 6)
-            sense.show_message(str(dice), text_colour=(255, 255, 255))
+            sense.show_message(
+                str(dice),
+                text_colour=(0, 255, 0)
+            )
 
 
