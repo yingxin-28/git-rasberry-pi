9b65a91f8cf27b454ee354ef1326214f4b22fc28
aa6097rg-rgb <aa6097rg@gmail.com>
2026-09-21 12:59:03 +0800

    added time

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import time
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
-flag=input("Do you want to continue? (Y/N): ")
-while flag=="Y":
+while True:
     temperature = sense.get_temperature()
     pressure = sense.get_pressure()
 
@@ -15,4 +15,4 @@ while flag=="Y":
         weather = "Cold"
         colour = [0,0,255]
     sense.show_message(f"{weather}, {temperature:.2f}C", text_colour = colour)
-    flag=input("Do you want to continue? (Y/N): ")
+    time.sleep(3)
