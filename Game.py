91dd79565fd4ee1dffbded646b46c8713a3ecc24
yingxin <inez.yingxin@gmail.com>
2026-09-21 15:05:50 +0800

    changed code

--- Game.py
+++ Game.py
@@ -1,15 +1,11 @@
+from sense_hat import SenseHat
 import random
-from sense_hat import SenseHat, ACTION_PRESSED
 sense = SenseHat()
-sense.clear()
 
-def roll_dice(event):
-    if event.action == ACTION_PRESSED:
-        number = random.randint(1, 6)
-        sense.show_message(f"Dice rolled: {number}", text_colour = (255,255,255))
-        if number==6:
-            sense.clear((0,255,0))  
-            sense.show_message("Six! One more roll", text_colour = (0,255,0))
-sense.stick.direction_middle = roll_dice
+while True:
+    for event in sense.stick.get_events():
+        if event.action == "pressed" and event.direction == "middle":
+            dice = random.randint(1, 6)
+            sense.show_message(str(dice), text_colour=(255, 255, 255))
 
 
