1a198ae6f8f91e4f2f2b06c8529305d9fe4e2304
yingxin <inez.yingxin@gmail.com>
2026-09-21 14:57:08 +0800

    added button and stuff

--- Game.py
+++ Game.py
@@ -1,7 +1,15 @@
 import random
-from sense_hat import SenseHat
+from sense_hat import SenseHat, ACTION_PRESSED
 sense = SenseHat()
 sense.clear()
 
-number = random.randint(1, 6)
-sense.show_message(f"Dice rolled: {number}", text_colour = (255,255,255))
+def roll_dice(event):
+    if event.action == ACTION_PRESSED:
+        number = random.randint(1, 6)
+        sense.show_message(f"Dice rolled: {number}", text_colour = (255,255,255))
+        if number==6:
+            sense.clear((0,255,0))  
+            sense.show_message("Six! One more roll", text_colour = (0,255,0))
+
+
+
