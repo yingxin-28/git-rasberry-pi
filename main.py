cbbab285d77740d939afd1f8ca297120dd560b6f
aa6097rg-rgb <aa6097rg@gmail.com>
2026-09-21 12:16:21 +0800

    print message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sense_hat import SenseHat
 sense = SenseHat()
+sense.clear()
+sense.set_pixel(0,0, 255, 255, 255)
 flag=input("Do you want to continue? (Y/N): ")
 while flag=="Y":
     temperature = sense.get_temperature()
@@ -7,4 +9,9 @@ while flag=="Y":
 
     print(f"Temperature {temperature:.2f} C")
     print(f"Pressure {pressure:.2f} hPa")
+    if temperature>30:
+        weather = "Hot"
+    else:
+        weather = "Cold"
+    sense.show_message(f"{weather}")
     flag=input("Do you want to continue? (Y/N): ")
