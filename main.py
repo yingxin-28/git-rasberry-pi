e3b91d7403c443982eec14f6628197181f045102
aa6097rg-rgb <aa6097rg@gmail.com>
2026-09-21 11:44:10 +0800

    edited code loop

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from sense_hat import SenseHat
 sense = SenseHat()
-temperature = sense.get_temperature()
-pressure = sense.get_pressure()
+flag=input("Do you want to continue? (Y/N): ")
+while flag=="Y":
+    temperature = sense.get_temperature()
+    pressure = sense.get_pressure()
 
-print("Temperature", temperature, "C")
-print("Pressure", pressure, "hPa")
+    print(f"Temperature {temperature:.2f} C")
+    print(f"Pressure {pressure:.2f} hPa")
+    flag=input("Do you want to continue? (Y/N): ")
